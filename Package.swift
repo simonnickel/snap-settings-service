ff2e87222658ca1ba5796a7b5b310fa712aec64b
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 11:59:35 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "snap-settings-service",
 	platforms: [
-		.iOS(.v17), .macOS(.v14) //, .tvOS(.v17)
+		.iOS(.v17), .macOS(.v14)
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
