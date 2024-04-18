5994f6c9f7e2e0430662f55e8d862330dd798a31
Simon Nickel <simonnickel@googlemail.com>
2024-04-18 19:44:12 +0200

    Add Demo project, created with Xcode 15.3

---
+++ SnapSettingsServiceDemo/SnapSettingsServiceDemo/SnapSettingsServiceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSettingsServiceDemoApp.swift
+//  SnapSettingsServiceDemo
+//
+//  Created by Simon Nickel on 18.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SnapSettingsServiceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
