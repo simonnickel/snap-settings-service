ba6092f47371797c6579c0da0998c7193083589f
Simon Nickel <simonnickel@googlemail.com>
2024-05-14 11:30:42 +0200

    Add comment

--- Sources/SnapSettingsService/Environment.swift
+++ Sources/SnapSettingsService/Environment.swift
@@ -10,6 +10,10 @@ import SwiftUI
 private struct SettingsServiceKey: EnvironmentKey {
 	
 	static var defaultValue: SettingsService {
+		// TODO concurrency: Instead of a `static let` this needs to be a computed property. Just applying @MainActor does not fullfill requirements for `EnvironmentKey` implementation.
+		// Not sure if this might be something that is solved in a future version of Swift or SwiftUI.EnvironmentKey.
+		// Static property 'defaultValue' is not concurrency-safe because it is not either conforming to 'Sendable' or isolated to a global actor; this is an error in Swift 6
+		// Main actor-isolated static property 'defaultValue' cannot be used to satisfy nonisolated protocol requirement
 		SettingsService(defaults: nil, ubiquitous: nil)
 	}
 	
